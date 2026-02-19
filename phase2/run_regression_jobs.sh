cc99adda88557b590d58f3eb1c74d60305b457be
Raphael Gibbs <gibbsr@nih.gov>
2026-02-19 16:18:09 -0500

    added bash scripts for running prep and regressions
    Added simple bash scripts for running the data prep and regressions
    
    This script are simple and the python script for generating was was more
    that the actual scripts themselves so deleted the bash script generation
    python code

---
+++ phase2/run_regression_jobs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "$(date): $(uname -n)"
+echo "Executing: $0 $@"
+
+REGRESSIONTYPE=$1 #"ols" "rlm"
+MODALITY=$2       #"rna" "atac"
+
+CELLTYPES="Astrocytes Endothelial ExN_BCL11B ExN_CUX2 ExN_LAMP5 ExN_RELN ExN_RMST ExN_RORB ExN_SEMA3E ExN_THEMIS InN_LAMP5 InN_PAX6 InN_PVALB InN_SST InN_VIP Microglia OPCs Oligodendrocytes"
+
+for CELLTYPE in ${CELLTYPES[@]}; do
+  echo "Running: uv run phase2/analyses/pseudobulk_regression.py --modality ${MODALITY} --cell-type ${CELLTYPE} --regression-type ${REGRESSIONTYPE}"
+  uv run phase2/analyses/pseudobulk_regression.py --modality ${MODALITY} --cell-type ${CELLTYPE} --regression-type ${REGRESSIONTYPE} &
+done
+
+wait
