62392f3024c931ec36dde7a6d8de6057eb8efc0d
Raphael Gibbs <gibbsr@nih.gov>
2026-03-06 16:54:57 -0500

    added variable looping
    added looping of variables instead of repearting all commands

--- phase2/run_analyses.sh
+++ phase2/run_analyses.sh
@@ -10,24 +10,29 @@ uv run phase2/analysis/format_covariates.py
 uv run phase2/analyses/pseudobulk_convert.py --aggregate-type sum
 
 # run the per cell-type pseudobulk data prep and generate non-target variance components
-phase2/run_prep_pb_jobs.sh prep_pb_data.py rna
-phase2/run_prep_pb_jobs.sh prep_pb_data.py atac
+MODALITIES="rna atac"
+for MODALITY in ${MODALITIES[@]}; do
+  phase2/run_prep_pb_jobs.sh prep_pb_data.py ${MODALITY}
+done
 
 # format the covariates to include the non-target variance components
 
 # run the variance partition analyis per cell-type
-phase2/run_prep_pb_jobs.sh run_variance_partition.py rna
-phase2/run_prep_pb_jobs.sh run_variance_partition.py atac
+for MODALITY in ${MODALITIES[@]}; do
+  phase2/run_prep_pb_jobs.sh run_variance_partition.py ${MODALITY}
+done
 
 # run the age regression analysis per cell-type
-phase2/run_regression_jobs.sh ols rna
-phase2/run_regression_jobs.sh ols atac
-phase2/run_regression_jobs.sh rlm rna
-phase2/run_regression_jobs.sh rlm atac
-phase2/run_regression_jobs.sh wls rna
-phase2/run_regression_jobs.sh wls atac
+REGRESSTYPES="wls ols rlm"
+for MODALITY in ${MODALITIES[@]}; do
+  for REGRESSTYPE in ${REGRESSTYPES[@]}; do
+    phase2/run_regression_jobs.sh ${REGRESSTYPE} ${MODALITY}
+  done
+done
 
 # post process the age regressions
-uv run phase2/analyses/post_pseudobulk_regression.py --modality rna --regression-type ols --no-volcano-per-celltype
-uv run phase2/analyses/post_pseudobulk_regression.py --modality rna --regression-type wls --no-volcano-per-celltype
-uv run phase2/analyses/post_pseudobulk_regression.py --modality rna --regression-type rlm --no-volcano-per-celltype
+for MODALITY in ${MODALITIES[@]}; do
+  for REGRESSTYPE in ${REGRESSTYPES[@]}; do
+    uv run phase2/analyses/post_pseudobulk_regression.py --modality ${MODALITY} --regression-type ${REGRESSTYPE} --no-volcano-per-celltype
+  done
+done
