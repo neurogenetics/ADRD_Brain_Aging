a849ea14532245df70e12e263949e8217295c027
Raphael Gibbs <gibbsr@nih.gov>
2026-03-09 17:47:47 -0400

    changes default for regression covariates to specified
    Changed the default for covariate selection when running the regression
    from using all the unkowns to the specified covariates based on the
    minimum number of PCAs suggested. Some celltypes were ending up with
    many covariates for this small sample size

--- phase2/analyses/post_pseudobulk_regression.py
+++ phase2/analyses/post_pseudobulk_regression.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import warnings
 from pathlib import Path
 import sys
 
--- phase2/run_regression_jobs.sh
+++ phase2/run_regression_jobs.sh
@@ -10,7 +10,7 @@ CELLTYPES="Astrocytes Endothelial ExN_BCL11B ExN_CUX2 ExN_LAMP5 ExN_RELN ExN_RMS
 
 for CELLTYPE in ${CELLTYPES[@]}; do
   echo "Running: uv run phase2/analyses/pseudobulk_regression.py --modality ${MODALITY} --cell-type ${CELLTYPE} --regression-type ${REGRESSIONTYPE}"
-  uv run phase2/analyses/pseudobulk_regression.py --modality ${MODALITY} --cell-type ${CELLTYPE} --regression-type ${REGRESSIONTYPE} --covariates unknowns &
+  uv run phase2/analyses/pseudobulk_regression.py --modality ${MODALITY} --cell-type ${CELLTYPE} --regression-type ${REGRESSIONTYPE} --covariates specified --covariates-list PCA_0 PCA_1 PCA_2 PCA_3 &
 done
 
 wait
